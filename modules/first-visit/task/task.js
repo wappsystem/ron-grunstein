d6678ff0b740c11f03cc84a1125a0b6cc44f6382
Gunnar Unger <gunnar.unger@gmail.com>
2018-03-12 17:09:01 +1100

    update

--- modules/first-visit/task/task.js
+++ modules/first-visit/task/task.js
@@ -37,6 +37,21 @@ $('#F__ID').submit(function(event){
     //--------------------------------------------------------
     event.preventDefault();
     $('#submit__ID').hide();
+    //if(alert_validation!="") {$vm.alert(alert_validation); return false;}
+    var module_name=$vm.vm["__ID"].name;
+    var db_pid=$vm.module_list[module_name].table_id
+    if(module_name=='first-visit_ess'){
+        $('#ESS__ID').val(parseInt($("input[name='ESS_1']:checked").val())+parseInt($("input[name='ESS_2']:checked").val())+parseInt($("input[name='ESS_3']:checked").val())+parseInt($("input[name='ESS_4']:checked").val())+parseInt($("input[name='ESS_5']:checked").val())+parseInt($("input[name='ESS_6']:checked").val())+parseInt($("input[name='ESS_7']:checked").val())+parseInt($("input[name='ESS_8']:checked").val()))
+    }
+    if(module_name=='first-visit_dass'){
+//        record.DASS_DS=((parseInt($("input[name='DASS_3']:checked").val())+parseInt($("input[name='DASS_5']:checked").val())+parseInt($("input[name='DASS_10']:checked").val())+parseInt($("input[name='DASS_13']:checked").val())+parseInt($("input[name='DASS_16']:checked").val())+parseInt($("input[name='DASS_17']:checked").val())+parseInt($("input[name='DASS_21']:checked").val()))*2).toString();
+//        record.DASS_AS=((parseInt($("input[name='DASS_2']:checked").val())+parseInt($("input[name='DASS_4']:checked").val())+parseInt($("input[name='DASS_7']:checked").val())+parseInt($("input[name='DASS_9']:checked").val())+parseInt($("input[name='DASS_15']:checked").val())+parseInt($("input[name='DASS_19']:checked").val())+parseInt($("input[name='DASS_20']:checked").val()))*2).toString();
+//        record.DASS_SS=((parseInt($("input[name='DASS_1']:checked").val())+parseInt($("input[name='DASS_6']:checked").val())+parseInt($("input[name='DASS_8']:checked").val())+parseInt($("input[name='DASS_11']:checked").val())+parseInt($("input[name='DASS_12']:checked").val())+parseInt($("input[name='DASS_14']:checked").val())+parseInt($("input[name='DASS_18']:checked").val()))*2).toString();
+        $('#DASS_DS__ID').val((parseInt($("input[name='DASS_3']:checked").val())+parseInt($("input[name='DASS_5']:checked").val())+parseInt($("input[name='DASS_10']:checked").val())+parseInt($("input[name='DASS_13']:checked").val())+parseInt($("input[name='DASS_16']:checked").val())+parseInt($("input[name='DASS_17']:checked").val())+parseInt($("input[name='DASS_21']:checked").val()))*2);
+        $('#DASS_AS__ID').val((parseInt($("input[name='DASS_2']:checked").val())+parseInt($("input[name='DASS_4']:checked").val())+parseInt($("input[name='DASS_7']:checked").val())+parseInt($("input[name='DASS_9']:checked").val())+parseInt($("input[name='DASS_15']:checked").val())+parseInt($("input[name='DASS_19']:checked").val())+parseInt($("input[name='DASS_20']:checked").val()))*2);
+        $('#DASS_SS__ID').val((parseInt($("input[name='DASS_1']:checked").val())+parseInt($("input[name='DASS_6']:checked").val())+parseInt($("input[name='DASS_8']:checked").val())+parseInt($("input[name='DASS_11']:checked").val())+parseInt($("input[name='DASS_12']:checked").val())+parseInt($("input[name='DASS_14']:checked").val())+parseInt($("input[name='DASS_18']:checked").val()))*2);
+    }
+
     //--------------------------------------------------------
     var data={}; var a=$("#F__ID").serializeArray(); $.each(a, function () { data[this.name]=this.value || '';});
     var dbv={}
